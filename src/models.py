0a5b717afc1493c43d16f73956a829fb1a53a0f1
dimcheck <dfarinyak@gmail.com>
2022-06-01 16:12:07 +0300

    added User model.

--- src/models.py
+++ src/models.py
@@ -1,10 +1,13 @@
+from enum import unique
 from . import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True)
+    name = db.Column(db.String(20))
     age = db.Column(db.Integer)
+    email = db.Column(db.String(40), unique=True)
+    password = db.Column(db.String(100))
 
     def __repr__(self) -> str:
         return '<User %r>' % self.name
