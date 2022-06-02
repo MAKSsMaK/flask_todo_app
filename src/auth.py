43fd13944a8213e6aaa9727784c1fc8b9838fd57
dimcheck <dfarinyak@gmail.com>
2022-06-02 17:00:37 +0300

    added validations.

--- src/auth.py
+++ src/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, flash, render_template, redirect, url_for, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user
 
 
 auth = Blueprint('auth', __name__)
@@ -10,6 +11,21 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash('Please check your login data you entered and try again.')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
 
 
 @auth.route('/register')
@@ -39,5 +55,7 @@ def register_post():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
