7a04188479598fe0ccd4a328ce421e20f07be3a5
dimcheck <dfarinyak@gmail.com>
2022-06-06 16:36:44 +0300

    moving to Pluggable views.

--- src/auth.py
+++ src/auth.py
@@ -1,61 +1,73 @@
-from flask import Blueprint, flash, render_template, redirect, url_for, request
-from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
-from . import db
-from flask_login import login_user, login_required, logout_user
+import typing as t
 
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask.views import MethodView
+from flask_login import login_required, login_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from . import db
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
-def login():
-    return render_template('login.html')
+class ShowLogin(MethodView):
+    def get(self):
+        return render_template('login.html')
+
+    def post(self):
+        email = request.form.get('email')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login data you entered and try again.')
+            return redirect(url_for('auth.show_login'))
+
+        login_user(user, remember=remember)
+        return redirect(url_for('main.show_profile'))
 
 
-@auth.route('/login', methods=['POST'])
-def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
+class ShowRegistration(MethodView):
+    def get(self):
+        return render_template('register.html')
 
-    user = User.query.filter_by(email=email).first()
-    if not user or not check_password_hash(user.password, password):
-        flash('Please check your login data you entered and try again.')
-        return redirect(url_for('auth.login'))
+    def post(self):
+        name = request.form.get('name')
+        age = request.form.get('age')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-    login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email address already exists')
+            return redirect(url_for('auth.show_register'))
 
+        new_user = User(name=name, age=age, email=email, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
 
-@auth.route('/register')
-def register():
-    return render_template('register.html')
-
-
-@auth.route('/register', methods=['POST'])
-def register_post():
-    name = request.form.get('name')
-    age = request.form.get('age')
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(email=email).first()
-
-    if user:
-        flash('Email address already exists')
-        return redirect(url_for('auth.register'))
-
-    new_user = User(name=name, age=age, email=email, password=generate_password_hash(password, method='sha256'))
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('auth.login'))
+        return redirect(url_for('auth_login'))
 
 
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('main.index'))
+class Logout(MethodView):
+    decorators: t.List[t.Callable] = [login_required]
+
+    def get(self):
+        logout_user()
+        return redirect(url_for('main.show_index'))
+
+
+login_view = ShowLogin.as_view('show_login')
+register_view = ShowRegistration.as_view('show_register')
+logout_view = Logout.as_view('logout')
+
+auth.add_url_rule('/login', view_func=login_view)
+auth.add_url_rule('/login', view_func=login_view, methods=['POST',])
+
+auth.add_url_rule('/register', view_func=register_view)
+auth.add_url_rule('/register', view_func=register_view, methods=['POST',])
+
+auth.add_url_rule('/logout', view_func=logout_view)
--- src/core.py
+++ src/core.py
@@ -1,16 +1,24 @@
+import typing as t
+
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from flask.views import MethodView
+from flask_login import current_user, login_required
 
 main = Blueprint('main', __name__)
 
 
 # a simple page that says hello
-@main.route('/')
-def index():
-    return render_template('index.html')
+class ShowIndex(MethodView):
+    def get(self):
+        return render_template('index.html')
 
 
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+class ShowProfile(MethodView):
+    decorators: t.List[t.Callable] = [login_required]
+
+    def get(self):
+        return render_template('profile.html', name=current_user.name)
+
+
+main.add_url_rule('/', view_func=ShowIndex.as_view('show_index'))
+main.add_url_rule('/profile', view_func=ShowProfile.as_view('show_profile'))
