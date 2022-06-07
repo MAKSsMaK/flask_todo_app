a4dd13c74c0d6bf7fa46cc87a65806dd6b1603f8
dimcheck <dfarinyak@gmail.com>
2022-06-07 14:06:12 +0300

    configured relationship

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
-from . import db
 from flask_login import UserMixin
+
+from . import db
 
 
 class User(UserMixin, db.Model):
@@ -8,6 +9,16 @@ class User(UserMixin, db.Model):
     age = db.Column(db.Integer)
     email = db.Column(db.String(40), unique=True)
     password = db.Column(db.String(100))
+    todos = db.relationship('Todo', backref='user')
 
     def __repr__(self) -> str:
         return '<User %r>' % self.name
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f'<Todo "{self.content[:20]}">'
